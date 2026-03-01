a47d2e3bd89a2149a38b88520443db85ccba4cf4
mburumaxwell <1645026+mburumaxwell@users.noreply.github.com>
2026-03-01 12:09:30 +0000

    Update azsqlidxmgr to 0.3.0

--- Formula/azsqlidxmgr.rb
+++ Formula/azsqlidxmgr.rb
@@ -5,29 +5,29 @@ class Azsqlidxmgr < Formula
   desc "Azure SQL Index Manager"
   homepage "https://github.com/mburumaxwell/azsqlidxmgr"
   license "MIT"
-  version "0.2.0"
+  version "0.3.0"
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/mburumaxwell/azsqlidxmgr/releases/download/0.2.0/azsqlidxmgr-0.2.0-linux-arm64.tar.gz"
-      sha256 "aa7499dfad46ac8170a60b4149043d112fd3d3570abfad703edbe0e562f89dfa"
+      url "https://github.com/mburumaxwell/azsqlidxmgr/releases/download/0.3.0/azsqlidxmgr-0.3.0-linux-arm64.tar.gz"
+      sha256 "a429a15a56081a1cf03efb326019574c7c3c8826ef2ae621bb62908872ca0d3b"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/mburumaxwell/azsqlidxmgr/releases/download/0.2.0/azsqlidxmgr-0.2.0-linux-x64.tar.gz"
-      sha256 "a158698129c6026a962af1a7f81f41863a8f8f9b8886aef727f31929ec68623f"
+      url "https://github.com/mburumaxwell/azsqlidxmgr/releases/download/0.3.0/azsqlidxmgr-0.3.0-linux-x64.tar.gz"
+      sha256 "b6740f35bbd2671e5626fda5b74182d98096ed5992f1a8bdd5638adcd9153f59"
     end
   end
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mburumaxwell/azsqlidxmgr/releases/download/0.2.0/azsqlidxmgr-0.2.0-osx-arm64.tar.gz"
-      sha256 "e7751a5b1752fc75d3df529394715d87d473ddc12efb8d477d38cb276716440b"
+      url "https://github.com/mburumaxwell/azsqlidxmgr/releases/download/0.3.0/azsqlidxmgr-0.3.0-osx-arm64.tar.gz"
+      sha256 "2a0c4a134224022a5c46e91c9fe997f56b2b65b2b78f3bf9d3748f6d417b4a85"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/mburumaxwell/azsqlidxmgr/releases/download/0.2.0/azsqlidxmgr-0.2.0-osx-x64.tar.gz"
-      sha256 "a07b1a6c346d24bce9812a5cacd1fbf8c5b7bed4f73faf210ace058dbe0975c4"
+      url "https://github.com/mburumaxwell/azsqlidxmgr/releases/download/0.3.0/azsqlidxmgr-0.3.0-osx-x64.tar.gz"
+      sha256 "8fa0b84f1a5d0e0ea07e4158b3db404ff26b1da2d68a70d28c1816c2aaa4e819"
     end
   end
 
