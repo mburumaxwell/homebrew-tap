de5bbae0933103ce4692498b78608ff94d3f4939
mburumaxwell <1645026+mburumaxwell@users.noreply.github.com>
2026-05-01 06:10:26 +0000

    Update azddns to 0.4.1

--- Formula/azddns.rb
+++ Formula/azddns.rb
@@ -5,29 +5,29 @@ class Azddns < Formula
   desc "Dynamic DNS (DDNS) tool for Azure DNS."
   homepage "https://github.com/mburumaxwell/azddns"
   license "MIT"
-  version "0.4.0"
+  version "0.4.1"
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/mburumaxwell/azddns/releases/download/0.4.0/azddns-0.4.0-linux-arm64.tar.gz"
-      sha256 "6eb3fd668f58ba81430b901ad5d7167791aad8fc72e41acb38a5331afd3e0738"
+      url "https://github.com/mburumaxwell/azddns/releases/download/0.4.1/azddns-0.4.1-linux-arm64.tar.gz"
+      sha256 "bbe43de78cddec8a567ef6abeef00234004cfa75f57a02c029aea1cc5aa1f798"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/mburumaxwell/azddns/releases/download/0.4.0/azddns-0.4.0-linux-x64.tar.gz"
-      sha256 "14b9cfea92cf8c00af624608d80855ab1514101d19dcd81dc48873abda5a2bc0"
+      url "https://github.com/mburumaxwell/azddns/releases/download/0.4.1/azddns-0.4.1-linux-x64.tar.gz"
+      sha256 "2c8bc33008e53a556dd7160be8d225cdb88fa7646b08a9a90d62f9b445ac6f03"
     end
   end
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mburumaxwell/azddns/releases/download/0.4.0/azddns-0.4.0-osx-arm64.tar.gz"
-      sha256 "9d6703995e9c13826c31d8c3f2d94c4e8354dccd2f5d945e6cbb534e1b4d92b7"
+      url "https://github.com/mburumaxwell/azddns/releases/download/0.4.1/azddns-0.4.1-osx-arm64.tar.gz"
+      sha256 "50e6c46387d635468d63e3c101892af1085df939cc30ea8347f2d4bff688104d"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/mburumaxwell/azddns/releases/download/0.4.0/azddns-0.4.0-osx-x64.tar.gz"
-      sha256 "fef9565127b79e7da3ef32aafc64f343c08fe232a53e98e4583e9f2af28e5ad3"
+      url "https://github.com/mburumaxwell/azddns/releases/download/0.4.1/azddns-0.4.1-osx-x64.tar.gz"
+      sha256 "9ae16500135cf8d5fd1f281b1476dfeeaa9a45a6b0c9150eff192621fd537c2c"
     end
   end
 
