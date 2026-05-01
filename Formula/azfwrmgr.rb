25c0ecc795e19bcdc2b75a1a3f34708c742c5064
mburumaxwell <1645026+mburumaxwell@users.noreply.github.com>
2026-05-01 05:56:35 +0000

    Update azfwrmgr to 0.3.1

--- Formula/azfwrmgr.rb
+++ Formula/azfwrmgr.rb
@@ -6,29 +6,29 @@ class Azfwrmgr < Formula
   desc "Azure Firewall Rules Manager."
   homepage "https://github.com/mburumaxwell/azfwrmgr"
   license "MIT"
-  version "0.3.0"
+  version "0.3.1"
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/mburumaxwell/azfwrmgr/releases/download/0.3.0/azfwrmgr-0.3.0-linux-arm64.tar.gz"
-      sha256 "b18bbc39fc9d7c6a2c62d1b74247b8b1f23706d09ec7c184ced7ae056f96568a"
+      url "https://github.com/mburumaxwell/azfwrmgr/releases/download/0.3.1/azfwrmgr-0.3.1-linux-arm64.tar.gz"
+      sha256 "52e6758eec578c19baebde37b92a2d70db533f57039af435a87cd2d1b1e8f3af"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/mburumaxwell/azfwrmgr/releases/download/0.3.0/azfwrmgr-0.3.0-linux-x64.tar.gz"
-      sha256 "772e295ec100dfb292d42222e4eeee80ea70a83e2e54a3fede0f92974b11edb2"
+      url "https://github.com/mburumaxwell/azfwrmgr/releases/download/0.3.1/azfwrmgr-0.3.1-linux-x64.tar.gz"
+      sha256 "4c0607dc20eec9072c075bc0bab081272a261c0d56befc3698d0aaaed086fa1b"
     end
   end
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mburumaxwell/azfwrmgr/releases/download/0.3.0/azfwrmgr-0.3.0-osx-arm64.tar.gz"
-      sha256 "51a82f701eeb91652af07acbd530ab4acf37b0a4c212ddee9cc92d36e040ac55"
+      url "https://github.com/mburumaxwell/azfwrmgr/releases/download/0.3.1/azfwrmgr-0.3.1-osx-arm64.tar.gz"
+      sha256 "d3cf06cb7d6255212591e53bd14f9f8466410f6975e8505ab66a28f8aaf5b235"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/mburumaxwell/azfwrmgr/releases/download/0.3.0/azfwrmgr-0.3.0-osx-x64.tar.gz"
-      sha256 "f09a223220502c7d67f6aa215a9cad2ee89aee7732dbfa743c58c0b3b95f19a2"
+      url "https://github.com/mburumaxwell/azfwrmgr/releases/download/0.3.1/azfwrmgr-0.3.1-osx-x64.tar.gz"
+      sha256 "978a63560b699708dae4c089bcadbe0cef4819a86b53083591dce2ae312ab987"
     end
   end
 
